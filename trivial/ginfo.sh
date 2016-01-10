951277ed4cac134f8729748ca1bc67d48478634d
Mark Bank <mbank@kent.edu>
2016-01-10 13:03:24 -0500

    general info script

---
+++ trivial/ginfo.sh
@@ -0,0 +1,10 @@
+#
+# script to print information of currently logged in user, + current date, time
+#
+clear
+echo "Hello $USER"
+echo -e "Today is \c ";date
+echo -e "Number of users logged in: \c"; who | wc -l
+echo "Calendar"
+cal
+exit 0
