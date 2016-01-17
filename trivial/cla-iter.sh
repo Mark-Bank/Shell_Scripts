d0bba9f74d3672fb7034c8f5d586505ed26701c8
Mark Bank <mbank@kent.edu>
2016-01-17 15:54:55 -0500

    fixed bug: entire file contents were passed as 1 arg. cla-iter lists argc

--- trivial/cla-iter.sh
+++ trivial/cla-iter.sh
@@ -2,6 +2,8 @@
 # command line argument iteration
 #
 
+echo "received $# args"
+
 for var in "$@"
 do
   echo "$var"
--- trivial/pipe-file-as-args.sh
+++ trivial/pipe-file-as-args.sh
@@ -8,8 +8,13 @@ then
   script=$2
   echo "args found in: $args"
   echo "script: $script"
-  ./$script "$(< $args)"
-  #./$script $args
-  exit 0
+  #./$script "$(< $args)"
+else
+  echo "you didn't supply the right number of args"
+  echo "use:"
+  echo "./pipe-file-as-args.sh <argument_file> <script_receiving_args>"
+  exit 1
 fi
 
+mapfile -t < $args
+./$script "${MAPFILE[@]}"
