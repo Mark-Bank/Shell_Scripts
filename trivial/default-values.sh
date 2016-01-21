1b9810fc468c249a17a0673aab8625b2af3268df
Mark Bank <mbank@kent.edu>
2016-01-21 15:39:34 -0500

    clarified default value syntax

--- trivial/default-values.sh
+++ trivial/default-values.sh
@@ -4,29 +4,8 @@
 
 echo "received $# args"
 
-var_a=${1:-default_a}
-var_b=${2:-default_b}
-var_c=${3:-default_c}
+var_a=${1-"default_a"}
+var_b=${2-"default_b"}
+var_c=${3-"default_c"}
 
 echo "vars: $var_a, $var_b, $var_c"
-
-#VAR_COUNT=0
-#TOTAL_VARS=5
-#while [ $VAR_COUNT -lt $TOTAL_VARS ]; do
-#  let VAR_COUNT=VARCOUNT+1
-#  printf -n "var"
-#  printf -n "$((VAR_COUNT)): "
-#  if [ $# -gt 0 ]; then
-#    printf "user-supplied ($1)"
-#    shift
-#  else
-#    printf "default-supplied
-
-
-
-#for var in "$@"
-#do
-#  echo "$var"
-#done
-
-#exit 0
