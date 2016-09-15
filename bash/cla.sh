c2f4855a53f788dbe2f322929242c7667bce2b66
mark.bank <mark.bank@ibm.com>
2016-09-15 16:03:45 -0400

    no magic numbers kthxbai

--- bash/cla.sh
+++ bash/cla.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # command line arguments and if/then/elseif/else demo
 #
+
+CHATTINESS_THRESHOLD=4
+
 clear
 echo "$USER is executing $0 under pid $$"
 echo "  and entered $# arguments"
@@ -8,7 +11,7 @@ echo "  and entered $# arguments"
 if [ "$#" = "0" ]
 then
   echo "$USER is pretty quiet today."
-elif [ "$#" -lt "4" ]
+elif [ "$#" -lt $CHATTINESS_THRESHOLD ]
 then
   echo "$USER is feeling pithy."
 else
